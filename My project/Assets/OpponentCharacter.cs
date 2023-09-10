56821f32b1728292abd983ded3e841cf0c51952e
mertCukadar <cukadar.mertkaan@gmail.com>
2023-09-10 23:11:20 +0300

    Opponent character cs file added not modify yet

---
+++ My project/Assets/OpponentCharacter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentCharacter : BaseCharacter
+{
+
+   
+    CharacterProperty objProp = new CharacterProperty(10f, 10f);
+    PlayerCharacter player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+    }
+}
