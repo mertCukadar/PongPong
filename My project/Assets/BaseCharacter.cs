1cb715ed3d646c6a643074ced53505e05021300e
mertCukadar <cukadar.mertkaan@gmail.com>
2023-09-10 10:34:10 +0300

    base character class added

--- My project/Assets/BaseCharacter.cs
+++ My project/Assets/BaseCharacter.cs
@@ -1,9 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class BaseCharacter : MonoBehaviour
 {
+
+
+    public struct CharacterProperty
+    {
+        public float speed;
+        public float hitForce;
+
+
+        //Constructer
+        public CharacterProperty(float speed, float hitForce)
+        {
+            this.speed = speed;
+            this.hitForce = hitForce;
+        }
+
+        
+
+    }
+
+
+    public virtual void Move(Rigidbody2D player, float speed, float xMove, float yMove)
+    {
+        player.velocity = new Vector2(xMove * speed, yMove * speed);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
